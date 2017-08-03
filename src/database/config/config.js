d8a3b13e4f19ab4c7d96848a3886b094e0a1f597
Johnny Chen <johnny.chen54@gmail.com>
2017-08-02 18:42:59 -0700

    Enable SSH for postgres

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -20,5 +20,11 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
   },
 };
