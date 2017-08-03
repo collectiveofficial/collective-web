8f8743aeb38ed852af74bc8e0873706b8a349433
coreykeyser <keyser.57@osu.edu>
2017-08-03 00:08:57 -0700

    big old UI changes

--- src/client/components/community/community.js
+++ src/client/components/community/community.js
@@ -19,7 +19,7 @@ class community extends Component {
           <div className={s.container}>
             <h1 className={s.banner}>Community</h1>
             <div className={s.card}>
-              Here is where we keep a pinterest like message board for food related topics, that sort of thing, newsletter, outreach
+              Thanks for coming to our community page. Here we will give you in the Collective community, a chance to connect with other bulk buyers, sharing recipes, organizing meetups, and setting up events built around food. We will keep you updated as we improve the site and make this the section that you want.
             </div>
           </div>
         </div>
