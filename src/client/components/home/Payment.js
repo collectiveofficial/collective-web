ba4d0ceed0e8a5b5f98e870d246746ba7a1eec65
Johnny Chen <johnny.chen54@gmail.com>
2017-08-14 11:41:00 -0500

    Input live publishable key

--- src/client/components/home/Payment.js
+++ src/client/components/home/Payment.js
@@ -192,7 +192,7 @@ class Payment extends React.Component {
                                     // panelLabel="Give Money" prepended to the amount in the bottom pay button
                                     amount={this.state.price * 100 + 50} // cents
                                     currency="USD"
-                                    stripeKey="pk_test_o6trMS2lojkAKMM0HbRJ0tDI"
+                                    stripeKey="pk_live_sJsPA40Mp18TUyoMH2CmCWIG"
                                     email="bestfoodforward@osu.edu"
                                     // Note: Enabling either address option will give the user the ability to
                                     // fill out both. Addresses are sent as a second parameter in the token callback.
