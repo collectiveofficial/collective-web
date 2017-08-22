057d301325ddfd7fbb6af5cd973c52016f376d16
coreykeyser <keyser.57@osu.edu>
2017-08-22 16:33:08 -0400

    fixing transparency

--- src/client/components/home/Payment.js
+++ src/client/components/home/Payment.js
@@ -203,13 +203,13 @@ class Payment extends React.Component {
                         <Modal.Content image>
                           <Modal.Description>
                             <br /><br />
-                            <p>Collective’s cutting edge automation streamlines the process for buyers and sellers to vote,</p>
-                            <p>organize and arrange payments for bulk buys. Our employees work around the clock to leverage</p>
-                            <p>the necessary technology and infraustructure to scale and provide the best produce to the most</p>
-                            <p>amount of people at an affordable price. Our infrastructure includes trusted payment providers,</p>
-                            <p>web hosting services and other partners to bring you the best experience possible. The total amount</p>
-                            <p> includes transaction fees for our payment providers and a small processing fee to cover web hosting</p>
-                            <p>and other infrastructure services. If you have any questions please contact us through our feedback <a href="https://docs.google.com/forms/d/e/1FAIpQLSdMJUSKNvto7jxcY800Z3ocrU7Hu7CSeu5B7M6s9ZJr7vGyzA/viewform?usp=sf_link" target="/blank">form</a>.</p>
+                            <p>Collective works to make organization easier for groups. And in order to meet</p>
+                            <p>these needs we must cover basic costs for insurance, web hosting, and card</p>
+                            <p>transaction fees. In order to legally use Stripe, our secure payment system</p>
+                            <p>we must cover a standard 2.9% + $.30 fee for every transaction ($.47 on every</p>
+                            <p>dorm package and $.68 on every cooking package). Our number one goal is to meet</p>
+                            <p>the needs of our customers, so we will do whatever we can to keep these prices</p>
+                            <p>low. But if you have any questions please contact us through our feedback <a href="https://docs.google.com/forms/d/e/1FAIpQLSdMJUSKNvto7jxcY800Z3ocrU7Hu7CSeu5B7M6s9ZJr7vGyzA/viewform?usp=sf_link" target="/blank">form</a>.</p>
                           </Modal.Description>
                         </Modal.Content>
                       </Modal>
