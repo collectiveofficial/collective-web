5daedfc573845c23827abaac2d6746bbd93a4e9c
coreykeyser <keyser.57@osu.edu>
2017-10-02 21:27:09 -0400

    final about and faq and payemnt changes

--- src/client/components/about/faq.js
+++ src/client/components/about/faq.js
@@ -15,63 +15,56 @@ class faq extends Component {
     return (
         <div className={s.rooter}>
           <div className={s.flexcontainer}>
-          <div className={s.card}>
-            <h2>Collective makes healthy food easy</h2>
-            <div className={s.infoCont}>
-              <div className={s.info}>We believe that the food system is too big, too inefficient, and too unsustainable to feed people in the way they want. So we help to easily connect independent groups to local sources, allowing consumers to empower themselves through high quality, cheap produce. By cutting out the middleman and getting back to the community, we can provide farmer's market quality at convenience store price. You just buy into our $6 or $11 packages, and we help to give you a box full of amazing food.</div>
-              <Image src='https://static1.squarespace.com/static/54ebb77ce4b05bee3a09f904/t/54ebbe9fe4b08f2aaf3f6563/1424735908416/FM.jpg?format=1500w' />
-           </div>
-         </div>
          <div className={s.card}>
-           <h2 className={s.head}>How it works</h2>
+           <h2 className={s.head}>Frequently Asked Questions</h2>
            <List>
              <List.Item>
-               <Image size="small" shape="circular" src='https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/17807497_1005883922845608_8912956886368631568_o.jpg?oh=6f07d42171594a4f73740bceee2962b7&oe=59EADBED' />
                <List.Content>
-                 <List.Header>Vote</List.Header>
-                 <List.Description>You vote and pay for the food you want</List.Description>
+                 <List.Header>How do I vote?</List.Header>
+                 <List.Description>Just create an account and then login. This will direct you to our home page that has all of our dates for upcoming bulk buys. To vote for that week, just hit vote now. Place your votes on that page then pay on the page after.</List.Description>
                </List.Content>
              </List.Item>
            </List>
            <List>
              <List.Item>
-               <Image size="small" shape="circular" src='https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/17854651_1005883912845609_4293592665208382319_o.jpg?oh=f0353b293d6f89d1b233eb0cc6e02a1a&oe=59EBC6F0' />
                <List.Content>
-                 <List.Header>Organize</List.Header>
-                 <List.Description>We compile votes, pool money, and then send everything back to independent organizers</List.Description>
+                 <List.Header>Why is it voting based and how long can I vote?</List.Header>
+                 <List.Description>We can save the most money and waste the least food if we can get everyone to roughly "agree" on what to buy. By doing this we don't have to "bet" on what foods will be bought like a grocery store does. Instead we jsut provide the highest quality food for the lowest possible price.</List.Description>
                </List.Content>
              </List.Item>
            </List>
            <List>
              <List.Item>
-               <Image size="small" shape="circular" src='https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/17855598_1005883899512277_3945066174219173922_o.jpg?oh=5a1f7d848a4efb9510a036481493bbf5&oe=59F30798' />
                <List.Content>
-                 <List.Header>Source</List.Header>
-                 <List.Description>Organizing groups buy the food directly from local sources</List.Description>
+                 <List.Header>Why pickups and can I pickup at another time?</List.Header>
+                 <List.Description>Our deliveries and pickups are volunteer run. So we don't currently have the resources to provide extra pickup windows. So when ordering, it is helpful if you can ensure that you (or a friend) can pick up at the set time. If you aren't able to pickup within 30 minutes of the dropoff we will be forced to donate the food to our charitable partners.</List.Description>
                </List.Content>
              </List.Item>
            </List>
            <List>
              <List.Item>
-               <Image size="small" shape="circular" src='https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/17880057_1005883869512280_2672757500595292705_o.jpg?oh=d896a7760c7feae53fd6dac05e9403db&oe=59F61E1A' />
                <List.Content>
-                 <List.Header>Community</List.Header>
-                 <List.Description>And then we work together to bring the bulk food to you, all for around half the cost of the grocery store</List.Description>
+                 <List.Header>What's a dorm bag and what's a cooking bag?</List.Header>
+                 <List.Description>We break up our orders into two options: a dorm bag, and a cooking bag. Dorm bag foods are fruit heavy and require no cooking. Our past dorm packages included 6 apples, 1 pound of carrots, 4 bananas, 6 kiwis, 1.5 lbs of grapes, and 5 oranges. Our cooking package is built for off-campus residents and is determined by uservote and nutrient quality. To give you an idea, a package in the past has included 3 sweet potatoes, 1 lb mini peppers, 1 lb tomatoes, 2 onions, 2 potatoes, 2 limes, 0.5 lb of spinach, 1 lb baby carrots.</List.Description>
                </List.Content>
              </List.Item>
            </List>
-         </div>
-         <div className={s.card}>
-           <h2 className={s.head}>Why it works</h2>
-           <div className={s.infoCont}>
-             <div className={s.info}>Our current food system relies on an inefficient supply chain that continually adds unnecessary costs to the food you end up buying. On top of that, grocery stores have become wasteful and inefficient. Even though you buy the same 30-40 products every single time you go to the store, you have to pay extra just to support the 10,000 other products that you have no interest in. And because grocery stores have to bet on what consumers are going to buy, they end up having to throw out billions of pounds of unbought food each year. By cutting out the middle man, and only providing the most ripe and healthy food, we ensure that our users are able to eat healthy for very little cost all while contributing to a more sustainable, low-waste food system.</div>
-             </div>
-         </div>
-         <div className={s.card}>
-           <h2 className={s.head}>Why we're better</h2>
-           <div className={s.infoCont}>
-             <div className={s.info}>We understand that many users have tons of options for where they can buy their groceries, we are certain that Collective is the best way. We are low cost and low waste and we work to support community farms and food organizers rather than to take them over. All across the baord, you can see companies like Amazon, Whole Foods, and Wal-Mart, who are monopolizing the food industry by taking over farms and setting predatory prices. Not only can they not meet our low prices or our sustainability, they don't work to empower community food systems like our communal purchasing system does. So if you are concerned about sustainability, waste, community, Collective is the only way to go.</div>
-           </div>
+           <List>
+             <List.Item>
+               <List.Content>
+                 <List.Header>How can I check whether my order was successful?</List.Header>
+                 <List.Description>If you have any questions about orders, your best bet is to refresh the page and then hit "Order Info" at the top of the header. This will automatically update and log any orders you've made in the past. If your order does not appear, we suggest retyping your information in the event that you madea typo on the first time around.</List.Description>
+               </List.Content>
+             </List.Item>
+           </List>
+           <List>
+             <List.Item>
+               <List.Content>
+                 <List.Header>Could I get a reciept?</List.Header>
+                 <List.Description>Every successful purchase will send a confirmation and reciept email to your account. On top of this, we also provide the "Order Info" page which will log all of your orders.</List.Description>
+               </List.Content>
+             </List.Item>
+           </List>
          </div>
        </div>
      </div>
